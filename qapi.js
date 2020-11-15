4b1767fa29c051e21ab73ec896ff4faf257ee5e8
olgalummyo <74501364+olgalummyo@users.noreply.github.com>
2020-11-15 18:04:40 +0200

    Changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb135458996cc001edce510",
+				"5fb135458996cc001edce512"
+			]
+		},
+		"5fb135458996cc001edce510": {
+			"id": "5fb135458996cc001edce510",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb135458996cc001edce512": {
+			"id": "5fb135458996cc001edce512",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
